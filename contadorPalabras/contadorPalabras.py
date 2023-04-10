84e3731a8a8801c1e6f6b2dc6a264eefeb10cdf7
Emiliano20011 <emilianomassolin@gmail.com>
2023-04-10 20:39:29 -0300

    terminado

--- contadorPalabras/contadorPalabras.py
+++ contadorPalabras/contadorPalabras.py
@@ -1,7 +1,13 @@
 
-def contadorPalabras(palabra):
-   listaDePalabras= palabra.split(" ")
-   numeroDePalabras=len(listaDePalabras)
-   return {numeroDePalabras}
+def contador(texto):
+    palabras = texto.split()
+    frecuencias = {}
+    for palabra in palabras:
+        if palabra in frecuencias:
+            frecuencias[palabra] += 1
+        else:
+            frecuencias[palabra] = 1
+    return frecuencias
+
 
 
--- contadorPalabras/test.py
+++ contadorPalabras/test.py
@@ -1,9 +1,12 @@
 import unittest
-from contadorPalabras import contadorPalabras
+from contadorPalabras import contador
 class TestCountLetters(unittest.TestCase):
     def test_simple(self):
-        result = contadorPalabras('hola')
+        result = contador("hola")
         self.assertEqual(result, {"hola":1})
+    def test_Dos(self):
+        result = contador("hola como como andas todo bien")
+        self.assertEqual(result, {"andas":1,"bien":1,"como":2,"hola":1,"todo":1})
 
 if __name__ == '__main__':
     unittest.main()
